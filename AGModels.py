4413137670e474f6be57e4b9754839aaa618760f
Alex Goodenbour <alg107@uclive.ac.nz>
2019-10-22 13:59:35 +1300

    added a few things

--- AGModels.py
+++ AGModels.py
@@ -10,6 +10,8 @@ TODO:
     - Add equivalent linear functions 
 """
 
+RES = 400
+
 def chisquare(obs, exp, error):
     chisqr = 0
     for i in range(len(obs)):
@@ -40,10 +42,16 @@ def plot_data(data, fignum, xlabel, ylabel):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
 
+def plot_model(data, fn, params):
+    ts= np.array(data[:,0])
+    ys = np.array(data[:,1])
+    t_arr = np.linspace(np.min(ts), np.max(ts), RES)
+    plt.plot(t_arr, fn(params, t_arr))
+
 def plot_model_data(data, fn, params):
     ts= np.array(data[:,0])
     ys = np.array(data[:,1])
-    t_arr = np.linspace(np.min(ts), np.max(ts), 400)
+    t_arr = np.linspace(np.min(ts), np.max(ts), RES)
     plot_data(data, 1, "x", "y")
     plt.plot(t_arr, fn(params, t_arr))
 
@@ -55,7 +63,7 @@ def calc_params_NL(guesses, pnts, fn):
     result = minimize(chi2,guesses, method='BFGS',args=(pnts, fn))
     param_unc = np.sqrt(2*np.diag(result.hess_inv))
     newparams = result.x
-    return newparams, param_unc[1]
+    return newparams, param_unc
 
 
 """
@@ -83,9 +91,23 @@ def dummy_data(fn, params, i, f, N, unc=0.05, jitter=0.1):
     uncs = np.array([random.uniform(-unc, unc)*a for a in y])
     return np.column_stack((t,y,uncs))
 
+# Rounds uncertainties to one significant figure
+def round_to_1(x):
+    return round(x, -int(np.floor(np.log10(abs(x)))))
+
+def present_params(ps, uncs, rounding):
+    if ps.size != uncs.size:
+        raise Exception("Parameters and uncertainties must have same size")
+    print("Fitted Parameters:")
+    for i,p in enumerate(uncs):
+        perc = 100*uncs[i]/ps[i]
+        print("Parameter "+ str(i+1) +": " ,end="")
+        print(round(ps[i], rounding), "±", round_to_1(uncs[i]), "("+str(round(perc,2))+"%)")
+
 def system_test(fn, params, i, f, guess_params):
-    data2 = dummy_data(fn, params, i, f, 500)
+    data2 = dummy_data(fn, params, i, f, RES)
     ps, uncs = proto_fit_NL(data2, fn, guess_params)
+    present_params(ps, uncs, 4)
     return ps, uncs
     
 
--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def gaussian(params, t):
     a,b,c = params
     return a*np.exp(-(t-b)**2) + c
 
+def parab2(params, t):
+    a,b,c = params
+    return a*t**2 + b*t + c
+
 def o():
     plt.gca().invert_yaxis()
 
@@ -34,4 +38,12 @@ AGModels.proto_fit_NL("KMTC15_I.pysis", mlens, guessparams, o)
 params = (2, 3, 1)
 guess_params = (3, 4, 0.5)
 ps, uncs = AGModels.system_test(gaussian, params, 0, 6, guess_params)
-print(ps)
+
+params2 = (1,3,2)
+guess_params2 = (0.8, 3.1, 2.2)
+data = AGModels.dummy_data(parab2, params, 0, 10, 100, unc=0.05, jitter=0.1)
+AGModels.plot_data(data, 3, "x", "y")
+ps2, uncs2 = AGModels.calc_params_NL(guess_params2, data, parab2)
+AGModels.plot_model(data, parab2, ps2)
+plt.show()
+AGModels.present_params(ps2, uncs2, 4)
